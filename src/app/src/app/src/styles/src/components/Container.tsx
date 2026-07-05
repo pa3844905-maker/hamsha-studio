eed7f87630efa8e30df62ed873c392852b71fd8c
pa3844905-maker <pa3844905@gmail.com>
2026-07-05 22:26:14 +0500

    Create Container.tsx

---
+++ src/app/src/app/src/styles/src/components/Container.tsx
@@ -0,0 +1,5 @@
+import React, { ReactNode } from 'react';
+
+export default function Container({ children }: { children: ReactNode }) {
+  return <div className="container-lg mx-auto">{children}</div>;
+}
